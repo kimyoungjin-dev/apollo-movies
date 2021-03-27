74e91a6f33a0cec30a4f57b9c76ee307c836b1b3
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-27 20:06:57 +0900

    Construct Home.js
    Create a separate header.js part in home.js and render it (code readability)
    
    css Design
    
    Transfer movie information to movie.js.

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,7 +1,76 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Loading from "Components/Loading";
+import Movie from "./Movie";
+import HomeHeader from "Components/HomeHeader";
+import { defaultImage } from "Components/utils";
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+  font-size: 30px;
+  font-weight: bold;
+  width: 100%;
+  padding-left: 15px;
+`;
+
+const Data = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 160px);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+`;
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      rating
+      description_intro
+      language
+      medium_cover_image
+      genres
+    }
+  }
+`;
 const Home = () => {
-  return <div></div>;
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(data);
+
+  return (
+    <Container>
+      <HomeHeader />
+      {loading ? (
+        <Loading loading={loading} />
+      ) : (
+        <>
+          <Title>Movies</Title>
+          <Data>
+            {data?.movies?.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                poster={movie.medium_cover_image}
+                title={movie.title}
+                description={movie.description_intro}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
+          </Data>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Home;
