bec66ddf2b596d3210c687393653a5764be52354
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-27 17:24:00 +0900

    Import GlobalStyles, and router.js.

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./Router";
+import GlobalStyles from "./GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Router />
+    </>
+  );
+};
+
+export default App;
