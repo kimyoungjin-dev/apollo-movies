e92125e2cde22f4d94810ba784a75cb0ed56966c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-27 20:05:46 +0900

    Overall, the background color is black and the color is white.

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -17,6 +17,8 @@ const globalStyles = createGlobalStyle`
     
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color:black;
+        color:white;
     }
 `;
 
